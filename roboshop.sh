f8be928a3ec40421ae977c639feaae37099b10fa
Satya Aruna <satya.aruna@gmail.com>
2026-02-01 00:19:43 +0530

    files changed

--- roboshop.sh
+++ roboshop.sh
@@ -3,6 +3,8 @@
 SG_ID="sg-05b94ced162e1d820"    # Security group ID  taken from AWS console
 AMI_ID="ami-0220d79f3f480ecf5"  # AMI ID taken from AWS console
 INSTANCE_TYPE="t3.micro"
+HOSTED_ZONEID="Z0542579J1GOAFY7EM5S"
+DOMAIN_NAME="asadaws2026.online"
 
 for instance in $@
 do
@@ -19,12 +21,39 @@ do
         --instance-ids $INSTANCE_ID \
         --query 'Reservations[*].Instances[*].[PublicIpAddress]' \
         --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances \
         --instance-ids $INSTANCE_ID \
         --query 'Reservations[*].Instances[*].[PrivateIpAddress]' \
         --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     fi
 
     echo "IP address: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id "$HOSTED_ZONEID" \
+    --change-batch '
+    {
+    "Comment": "Update record to reflect new IP address",
+    "Changes": [
+        {
+        "Action": "UPSERT",
+        "ResourceRecordSet": {
+            "Name": "'$RECORD_NAME'",
+            "Type": "A",
+            "TTL": 1,
+            "ResourceRecords": [
+            {
+                "Value": "'$IP'"
+            }
+            ]
+        }
+        }
+    ]
+    }'
+
+    echo "Record updated for $instance"
+
 done
