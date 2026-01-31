411bc4c36647859b1a05138ca4da594b4cd31390
Satya Aruna <satya.aruna@gmail.com>
2026-01-31 23:40:58 +0530

    files changed

--- roboshop.sh
+++ roboshop.sh
@@ -7,22 +7,23 @@ INSTANCE_TYPE="t3.micro"
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances \
-                    --image-id $AMI_ID \
-                    --instance-type $INSTANCE_TYPE \
-                    --security-group-ids $SG_ID \
-                    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$insance}]' \
-                    --query 'Instances[0].InstanceID' \
-                    --output text)
+    --image-id $AMI_ID \
+    --instance-type $INSTANCE_TYPE \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$insance}]" \
+    --query 'Instances[0].InstanceID' \
+    --output text)   
+
     if [ $instance == 'frontend' ]; then
         IP=$(aws ec2 describe-instances \
-            --instance-ids $INSTANCE_ID \
-            --query 'Reservations[*].Instances[*].[PublicIpAddress]' \
-            --output text)
+        --instance-ids $INSTANCE_ID \
+        --query 'Reservations[*].Instances[*].[PublicIpAddress]' \
+        --output text)
     else
         IP=$(aws ec2 describe-instances \
-            --instance-ids $INSTANCE_ID \
-            --query 'Reservations[*].Instances[*].[PrivateIpAddress]' \
-            --output text)
+        --instance-ids $INSTANCE_ID \
+        --query 'Reservations[*].Instances[*].[PrivateIpAddress]' \
+        --output text)
     fi
 
     echo "IP address: $IP"
